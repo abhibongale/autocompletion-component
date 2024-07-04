6cb8b8f21d08e9b7284097970fd15593ea6bc929
abhibongale <abhishekbongale@outlook.com>
2024-07-04 16:05:47 +0100

    2. Implemented the Frontend
    * Added the UI bare-bone logic
    * Tested the bare-bone logic

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/css/App.css';
+import SongSearch from './components/SongSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SongSearch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/SongSearch.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../assets/css/SongSearch.css'; // Import CSS file for styles
+import Heading from './Heading';
+import FilterSection from './FilterSection';
+import Container from '@mui/material/Container';
+
+
+// Set default base URL for Axios requests
+axios.defaults.baseURL = 'http://localhost:8000';
+
+const SongSearch = () => {
+  // State variables for managing query, error, search results, and filters
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [filters, setFilters] = useState({
+    artistName: true,
+    albumName: true,
+    title: true,
+  });
+
+  useEffect(() => {
+    const delaySearch = setTimeout(() => {
+      handleSearch();
+    }, 15); // Delay in milliseconds
+
+    return () => clearTimeout(delaySearch); // Cleanup clearTimeout on component unmount or re-trigger
+  }, [query, filters]);
+
+
+  // Function to handle changes in filter checkboxes
+  const handleFilterChange = (event) => {
+    const { name, checked } = event.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: checked,
+    }));
+  };
+
+  // Function to handle search based on selected filters
+  const handleSearch = async () => {
+    try {
+      const params = {}
+
+      // Add parameters based on active filters
+      if (filters.artistName) params.authorName = query
+      if (filters.albumName) params.albumName = query
+      if (filters.title) params.title = query
+
+      // Make GET request to API endpoint
+      const response = await axios.get('/api/search', {params});
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error('Error searching songs:', error);
+      setSearchResults([]); // Clear previous results on error
+      setError('An error occurred while searching songs.');
+    }
+  };
+
+  // Function to toggle expanded details for a specific search result
+  const toggleDetails = (index) => {
+    const updatedResults = [...searchResults];
+    updatedResults[index].expanded = !updatedResults[index].expanded;
+    setSearchResults(updatedResults);
+  };
+
+  // JSX structure for rendering the SongSearch component
+  return (
+    <Container maxWidth={false} disableGutters>
+      <Heading/>
+      <div className="search-input">
+        <label>Search: </label>
+        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+      </div>
+      <FilterSection filters = {filters} handleFilterChange = {handleFilterChange} />
+      {error && <p className="error-message">{error}</p>}
+      <div className="search-results">
+        <h2>Suggestion:</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Artist Name</th>
+              <th>Album Name</th>
+              <th>Title</th>
+              <th>Length</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {searchResults.map((song, index) => (
+              <React.Fragment key={index}>
+                <tr>
+                  <td>{song.artist}</td>
+                  <td>{song.album}</td>
+                  <td>{song.title}</td>
+                  <td>{song.length}</td>
+                  <td>
+                    <button onClick={() => toggleDetails(index)}>
+                      {song.expanded ? 'Hide Details' : 'Show Details'}
+                    </button>
+                  </td>
+                </tr>
+                {song.expanded && (
+                  <tr className="details-row">
+                    <td colSpan="6">
+                      <div className="details">
+                        <p><strong>Description:</strong> {song.description}</p>
+                      </div>
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Container>
+  );
+};
+
+export default SongSearch;
